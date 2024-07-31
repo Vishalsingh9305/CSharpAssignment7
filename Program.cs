6bd4b70fc36c20a4c8cdb46754cf34e89d95f97f
HP <HP@DESKTOP-2L57IV8>
2024-07-31 10:25:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Assignment7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 5, 3, -6, -4, 10, 2 };
+            //int[] A = [-1, -3];
+            //int[] A = [1, 2, 3];
+
+            Solution solution = new Solution();
+
+            int min = solution.solution(A);
+
+            Console.WriteLine("Square of Minimum Positive Integer is: " + (min * min));
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignment7
+{
+    internal class Solution
+    {
+        public int solution(int[] A)
+        {
+            int min = int.MaxValue;
+            bool foundPositive = false;
+
+            foreach (int num in A)
+            {
+                if (num > 0)
+                {
+                    min = Math.Min(min, num);
+                    foundPositive = true;
+                }
+            }
+
+            return foundPositive ? min : 0;
+        }
+    }
+}
